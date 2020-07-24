46bf03578a8f49eee2de28a0091d955ad748fdc9
Richard Medina <rmedina@hawaii.edu>
2020-07-23 16:31:11 -1000

    cleanup

--- functions.php
+++ functions.php
@@ -3,3 +3,6 @@ function divi__child_theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
 add_action( 'wp_enqueue_scripts', 'divi__child_theme_enqueue_styles' );
+
+// Possibly not needed -- deprecate
+wp_localize_script( 'ajax-search', 'ajaxurl', admin_url( 'admin-ajax.php' ) );
